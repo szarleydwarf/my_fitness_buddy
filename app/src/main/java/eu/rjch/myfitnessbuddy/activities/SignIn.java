765a1598c20508293a7a14ee3312e2c348b51d85
RJ <szarleydwarf@gmail.com>
2019-05-29 12:41:34 +0100

    fixing issue with the sign in layout it was the field.xml which was causing the problem file removed and bw_box.xml created instead

--- app/src/main/java/eu/rjch/myfitnessbuddy/activities/SignIn.java
+++ app/src/main/java/eu/rjch/myfitnessbuddy/activities/SignIn.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import eu.rjch.myfitnessbuddy.R;
 import eu.rjch.myfitnessbuddy.utility.Utilities;
@@ -21,14 +22,28 @@ public class SignIn extends Activity {
         Utilities u = new Utilities();
         u.showToast(this, "Sign in please");
 
-        Button eulabtn = findViewById(R.id.eula_btn);
-        eulabtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(view.getContext(), Eula.class);
-                startActivity(i);
-                finish();
-            }
-        });
+//        initiateBtns(u);
+
+        passwordIconToggle(u);
+    }
+
+    private void passwordIconToggle(Utilities u) {
+//        ImageView cros_eye = findViewById(R.id.inside_imageview);
+//        ImageView reg_eye = findViewById(R.id.outside_imageview);
+
+//        u.iconToggle(reg_eye, cros_eye);
+//        u.iconToggle(cros_eye);
+    }
+
+    private void initiateBtns(Utilities u) {
+//        Button eulabtn = findViewById(R.id.eula_btn);
+//        eulabtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent i = new Intent(view.getContext(), Eula.class);
+//                startActivity(i);
+//                finish();
+//            }
+//        });
     }
 }
--- app/src/main/java/eu/rjch/myfitnessbuddy/utility/Utilities.java
+++ app/src/main/java/eu/rjch/myfitnessbuddy/utility/Utilities.java
@@ -3,9 +3,11 @@ package eu.rjch.myfitnessbuddy.utility;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -41,5 +43,37 @@ public class Utilities {
         t.setGravity(Gravity.CENTER_VERTICAL,0 ,0);
         t.setView(ctv);
         t.show();
+    }
+
+    public void iconToggle(final ImageView iv1, final ImageView iv2) {
+        iv1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(iv1.getVisibility() == View.VISIBLE){
+                    iv1.setVisibility(View.INVISIBLE);
+                    iv2.setVisibility(View.VISIBLE);
+                    Log.d("WWW", "it "+1+" visible");
+                } else {
+                    iv1.setVisibility(View.VISIBLE);
+                    iv2.setVisibility(View.INVISIBLE);
+                    Log.d("WWW", "iconToggle "+iv1.getId()+" invisible");
+                }
+            }
+        });
+    }
+
+    public void iconToggle(final ImageView iv) {
+        iv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(iv.getVisibility() == View.VISIBLE){
+                    iv.setImageResource(R.drawable.showeye);
+                    Log.d("WWW", "iT  visible");
+                } else {
+                    iv.setImageResource(R.drawable.hideeye);
+                    Log.d("WWW", "iT invisible");
+                }
+            }
+        });
     }
 }
