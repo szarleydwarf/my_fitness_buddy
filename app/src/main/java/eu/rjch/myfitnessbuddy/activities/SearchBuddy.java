6d4c16b3162336873c71c93b715b088b11b72ab9
RJ <szarleydwarf@gmail.com>
2019-06-04 16:23:59 +0100

    adding google sign in option

---
+++ app/src/main/java/eu/rjch/myfitnessbuddy/activities/SearchBuddy.java
@@ -0,0 +1,20 @@
+package eu.rjch.myfitnessbuddy.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import eu.rjch.myfitnessbuddy.R;
+
+public class SearchBuddy extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search_buddy_layout);
+        intit();
+    }
+
+    private void intit() {
+
+    }
+
+}
--- app/src/main/java/eu/rjch/myfitnessbuddy/activities/SignIn.java
+++ app/src/main/java/eu/rjch/myfitnessbuddy/activities/SignIn.java
@@ -11,10 +11,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+
 import eu.rjch.myfitnessbuddy.R;
 import eu.rjch.myfitnessbuddy.utility.Utilities;
 
 public class SignIn extends Activity {
+    private GoogleSignInClient mGoogleSignInClient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,9 +32,36 @@ public class SignIn extends Activity {
     private void intit() {
         Utilities u = new Utilities();
 
+        // Check for existing Google Sign In account,
+        // if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+//todo If GoogleSignIn.getLastSignedInAccount returns a GoogleSignInAccount
+// object (rather than null), the user has already signed in to your app
+// with Google. Update your UI accordingly—that is, hide the sign-in button,
+// launch your main activity, or whatever is appropriate for your app.
+// If GoogleSignIn.getLastSignedInAccount returns null,
+// the user has not yet signed in to your app with Google.
+// Update your UI to display the Google Sign-in button
+        if(account == null)
+            setGoogleSignin();
+        else
+            runApp();
         initiateBtns(u);
 
         passwordIconToggle(u);
+    }
+
+    private void runApp() {
+        Intent i = new Intent(this, SearchBuddy.class);
+    }
+
+    private void setGoogleSignin() {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+
     }
 
     private void passwordIconToggle(final Utilities u) {
