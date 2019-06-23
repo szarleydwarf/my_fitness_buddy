93c06926b30a36536240ef32beb461e9c41ff69f
RJ <szarleydwarf@gmail.com>
2019-06-23 10:26:32 +0100

    signing in works, some issues with signing out options

--- app/src/main/java/eu/rjch/myfitnessbuddy/activities/MyProfileAct.java
+++ app/src/main/java/eu/rjch/myfitnessbuddy/activities/MyProfileAct.java
@@ -1,13 +1,29 @@
 package eu.rjch.myfitnessbuddy.activities;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.squareup.picasso.Picasso;
 
 import eu.rjch.myfitnessbuddy.R;
 import eu.rjch.myfitnessbuddy.utility.Utilities;
 
-public class MyProfileAct extends Activity {
+public class MyProfileAct extends Activity implements View.OnClickListener {
+    private String email, username, photo;
+    private Uri url;
+    private TextView userTV, emailTV;
+    private ImageView iv;
+    private Button signout_btn;
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +34,46 @@ public class MyProfileAct extends Activity {
 
     private void intit() {
         Utilities u = new Utilities();
-        u.showToast(this,"Hello User");
+
+        mAuth = FirebaseAuth.getInstance();
+
+        signout_btn = findViewById(R.id.signout_btn);
+        signout_btn.setOnClickListener(this);
+
+        email = getIntent().getStringExtra("email");
+        username = getIntent().getStringExtra("username");
+        photo = getIntent().getStringExtra("url");
+
+        u.showToast(this,"Hello " + username);
+        url = Uri.parse(photo);
+
+        userTV = findViewById(R.id.user_nick);
+        emailTV = findViewById(R.id.user_email);
+        iv = findViewById(R.id.photo);
+
+        userTV.setText(username);
+        emailTV.setText(email);
+
+        Picasso.get().load(url).into(iv);
+    }
+    public void onClick(View view) {
+        goBack();
+    }
+
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser == null)
+            goBack();
+    }
+
+    private void goBack() {
+        FirebaseAuth.getInstance().signOut();
+        Intent i = new Intent(this, SignIn.class);
+        i.putExtra("signout", true);
+        startActivity(i);
+        finish();
     }
 
 }
--- app/src/main/java/eu/rjch/myfitnessbuddy/activities/SignIn.java
+++ app/src/main/java/eu/rjch/myfitnessbuddy/activities/SignIn.java
@@ -3,69 +3,117 @@ package eu.rjch.myfitnessbuddy.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
+import android.widget.Toast;
+
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 import eu.rjch.myfitnessbuddy.R;
 import eu.rjch.myfitnessbuddy.utility.Utilities;
 
 
-public class SignIn extends Activity {
+public class SignIn extends Activity implements View.OnClickListener {
 
     private Utilities u;
+    private static final int MY_REQUEST_CODE = 22;
+    private List<AuthUI.IdpConfig> providers;
+    private Button signout;
+    private boolean signedout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signin_layout);
+        signedout = getIntent().getBooleanExtra("signout", false);
         init();
     }
 
     private void init(){
-        initiateBtns();
+        signout = findViewById(R.id.sign_out_btn);
+        signout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AuthUI.getInstance().signOut(SignIn.this)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                signout.setEnabled(false);
+                                showSigningOptions();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(SignIn.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+        providers = Arrays.asList(
+                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                new AuthUI.IdpConfig.FacebookBuilder().build(),
+                new AuthUI.IdpConfig.EmailBuilder().build()
+
+        );
+
+        showSigningOptions();
     }
 
-
-    private void initiateBtns() {
-        Button eulabtn = findViewById(R.id.eula_btn);
-        eulabtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                runActivity(Eula.class);
-
-            }
-        });
-
-        ImageButton googleBtn = findViewById(R.id.g_sign_btn);
-        googleBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                runActivity(GSignIn.class);
-            }
-        });
-
-        ImageButton fbBtn = findViewById(R.id.fb_sign_btn);
-        fbBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                runActivity(FSigniIn.class);
-            }
-        });
-
-        Button emailSignBtn = findViewById(R.id.email_signin_btn);
-        emailSignBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //todo test if pasword and email views not empty
-                runActivity(EmailSignIn.class);
-            }
-        });
+    private void showSigningOptions() {
+        int requestCode;
+        if(signedout) {
+            signout.setEnabled(true);
+            requestCode = 0;
+        } else {
+            requestCode = MY_REQUEST_CODE;
+        }
+        startActivityForResult(
+                AuthUI.getInstance().createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .setTheme(R.style.MyTheme)
+                        .build(), requestCode
+        );
     }
 
-    private void runActivity(Class<?> classy) {
-        Intent i = new Intent(this, classy);
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == MY_REQUEST_CODE) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if(resultCode == RESULT_OK){
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                signIn(user);
+                Toast.makeText(this, ""+user.getEmail(), Toast.LENGTH_SHORT).show();
+                signout.setEnabled(true);
+            } else {
+                Toast.makeText(this, ""+response.getError().getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void signIn(FirebaseUser user) {
+        Intent i = new Intent(this, MyProfileAct.class);
+        i.putExtra("email", user.getEmail());
+        i.putExtra("username", user.getDisplayName());
+        i.putExtra("url", String.valueOf(user.getPhotoUrl()));
         startActivity(i);
         finish();
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
